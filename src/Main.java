c97850ff5b5747d6a49c81ff3e52c83f34084b4d
Iury Chagas <iurychagas@icloud.com>
2020-01-14 12:48:04 -0300

    seção 3 - aula 18 - entrada de dados - double

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
+
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 		
-		int value;
+		double value;
 		
-		value = input.nextInt();
+		value = input.nextDouble();
 		System.out.println("Valor digitador: "+ value);
 		
 		input.close();
+		// obs: java pega a localidade do sistema ou seja, se for BR: a declação devera ser utilizando virgulas!
+		// obs: para considerar o separador de decimais como ponto, ANTES da declacação do Scanner, faça:
+		// Locale.setDefault(locale.US);
 	}
 
 }
