23542062739f31bee94930b0802252028d3bc521
Iury Chagas <iurychagas@icloud.com>
2020-01-14 11:49:03 -0300

    add seção 3 - aula 16. - Estrutura Sequencial

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		String name = "Marciano";
+		int age = 31;
+		double income = 5000;
 		
+		System.out.printf("%s tem %d anos e seu salário atual é: R$%.2f%n", name, age, income);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%s tem %d anos e seu salário atual é: R$%.2f%n", name, age, income);
+		System.out.println("\n Nome: "+name+"\n Idade: "+age+"\n Salário: "+income);
 	}
 
 }
