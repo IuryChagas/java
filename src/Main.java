a50aa33451455713cedcc708416e645acc1d1b9f
Iury Chagas <iurychagas@icloud.com>
2020-01-14 12:13:01 -0300

    add Exercício de fixação

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,26 @@ import java.util.Locale;
 public class Main {
 
 	public static void main(String[] args) {
-		String name = "Marciano";
-		int age = 31;
-		double income = 5000;
+		System.out.println("Products: \n");
 		
-		System.out.printf("%s tem %d anos e seu salário atual é: R$%.2f%n", name, age, income);
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		String gender = "F";
+		
+		int age = 27;
+		int code = 5290;
+		
+		double price1 = 2100.00;
+		double price2 = 650.00;
+		double measure = 53.234567;
+		
+		System.out.printf("%s, whitch price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, whitch price is $ %.2f%n%n", product2, price2);
+		System.out.printf("Record: %d years old, %d and %s:%n%n", age, code, gender);
+		System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
 		Locale.setDefault(Locale.US);
-		System.out.printf("%s tem %d anos e seu salário atual é: R$%.2f%n", name, age, income);
-		System.out.println("\n Nome: "+name+"\n Idade: "+age+"\n Salário: "+income);
+		System.out.printf("US decimal point: %.3f%n%n", measure);
 	}
 
 }
