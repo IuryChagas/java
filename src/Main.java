a6705c9622ae6d04571ec64097247762d4c970f1
Iury Chagas <iurychagas@icloud.com>
2020-01-14 12:52:24 -0300

    seção 3 - aula 18 - entrada de dados - caracteres

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,14 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 		
-		double value;
+		char value;
 		
-		value = input.nextDouble();
+		value = input.next().charAt(0);
 		System.out.println("Valor digitador: "+ value);
 		
 		input.close();
-		// obs: java pega a localidade do sistema ou seja, se for BR: a declação devera ser utilizando virgulas!
-		// obs: para considerar o separador de decimais como ponto, ANTES da declacação do Scanner, faça:
-		// Locale.setDefault(locale.US);
 	}
 
 }
