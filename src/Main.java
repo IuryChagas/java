fb9a2815b0a391a6018dc90242320e3836bf3da0
Iury Chagas <iurychagas@icloud.com>
2020-01-14 13:40:07 -0300

    add  seção 3 - aula 19 - entrada de dados - Como limpar o buffer de leitura

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,40 @@ public class Main {
 
 		Scanner input = new Scanner(System.in);
 		
+		int z;
 		String s1, s2, s3;
 		
+		 z = input.nextInt();
+		
+			 input.nextLine();
 		s1 = input.nextLine();
 		s2 = input.nextLine();
 		s3 = input.nextLine();
 		
 		System.out.println("DADOS DIGITADOS: \n");
+		
+		System.out.println(z);
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println(s3);
 
 		input.close();
+		
+		//	### Observação! ###
+		//
+		// 							problema!
+		//
+		// Quando você usa um comando de leitura diferente do nextLine()
+ 		// e dá alguma quebra de linha, essa quebra de linha fica "pendente" na entrada padrão.
+		// 
+		// Se você então fizer um nextLine(),
+		// aquela quebra de linha pendente será absorvida pelo nextLine().
+		// 
+		//							Solução!
+		//
+		// Faça um nextLine() extra antes de fazer o nextLine() de seu interesse. 
+		//
+
 	}
 
 }
