a3c5403b530e40b23aa42cc93393fa95895259cc
Iury Chagas <iurychagas@icloud.com>
2020-01-15 13:28:46 -0300

    add exerício uri1007

---
+++ uri_online_judge/src/uri1007.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class uri1007 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int A, B, C, D, DIFERENCA;
+		
+		A = input.nextInt();
+		B = input.nextInt();
+		C = input.nextInt();
+		D = input.nextInt();
+		
+		DIFERENCA = (A * B - C * D);
+		
+		System.out.println("DIFERENÇA = "+ DIFERENCA);
+	}
+
+}
