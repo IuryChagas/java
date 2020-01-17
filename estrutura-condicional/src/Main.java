72704ff865ff9e7c5ff723078abbab373c951b83
Iury Chagas <iurychagas@icloud.com>
2020-01-17 10:58:07 -0300

    add Exercício resolvido 03 - Leitor de menor valor

--- estrutura-condicional/src/Main.java
+++ estrutura-condicional/src/Main.java
@@ -1,28 +1,26 @@
-// leia duas notas do aluno, mostre a nota final com observação se reprovado caso nota seja inferior à 60.0! 
-
-import java.util.Locale;
+// leia três números inteiros. Mostre qual é o menor dentre os três números lidos.
+// Em caso de empate mostre apenas uma vez.
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
-			double nota1, nota2, notaFinal;
+			int n1, n2, n3, menor = 0;
 			
-			System.out.println("1° Nota do Aluno: \n");
-			nota1 = input.nextDouble();
+			n1 = input.nextInt();
+			n2 = input.nextInt();
+			n3 = input.nextInt();
 			
-			System.out.println("2° Nota do Aluno: \n");
-			nota2 = input.nextDouble();
-			
-			notaFinal = (nota1 + nota2);
-			
-			if (notaFinal > 60.0) {
-				System.out.printf("NOTA FINAL = %.1f%n", notaFinal);
-			} else {
-				System.out.printf("NOTA FINAL = %.1f%nREPROVADO", notaFinal);
+			if(n1 < n2 && n1 < n3){
+				menor = n1;
+			}else if(n2 < n1 && n2 < n3){
+				menor = n2;
+			}else {
+				menor = n3;
 			}
+			
+			System.out.printf("MENOR = %d%n", menor);
 		input.close();
 	}
 
