7786774848d34728688c68baffed71f09fb97fd1
Iury Chagas <iurychagas@icloud.com>
2020-03-20 09:18:32 -0300

    add repository

---
+++ mvc-java/cobranca/src/main/java/com/gft/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.gft.cobranca.repository;
+
+import com.gft.cobranca.model.Titulo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface Titulos extends JpaRepository<Titulo, Long> {
+
+}
