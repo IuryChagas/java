889d9a5dec9dd6c15ca9be9a5a7c7243f1f000e0
Iury Chagas <iurychagas@icloud.com>
2020-03-20 09:19:36 -0300

    add register a new title

--- mvc-java/cobranca/src/main/java/com/gft/cobranca/CobrancaApplication.java
+++ mvc-java/cobranca/src/main/java/com/gft/cobranca/CobrancaApplication.java
@@ -1,7 +1,11 @@
 package com.gft.cobranca;
 
+import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.LocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
 public class CobrancaApplication {
@@ -10,4 +14,8 @@ public class CobrancaApplication {
 		SpringApplication.run(CobrancaApplication.class, args);
 	}
 
+	@Bean
+	public FixedLocaleResolver localeResolve() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
 }
--- mvc-java/cobranca/src/main/java/com/gft/cobranca/controller/TituloController.java
+++ mvc-java/cobranca/src/main/java/com/gft/cobranca/controller/TituloController.java
@@ -1,15 +1,20 @@
 package com.gft.cobranca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.gft.cobranca.model.Titulo;
+import com.gft.cobranca.repository.Titulos;
 
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+
+	@Autowired
+	private Titulos titulos;
 
     @RequestMapping("/novo")
     public String novo() {
