bd9fabdf133f87cfbfaecf378adf431b896542ec
Iury Chagas <iurychagas@icloud.com>
2020-04-06 11:00:26 -0300

    seção 4: Exercício 02

--- Java-Intensive-hands-on/curso_programacao/src/Main.java
+++ Java-Intensive-hands-on/curso_programacao/src/Main.java
@@ -1,19 +1,26 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+//		Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+//		casas decimais conforme exemplos.
+//
+//		Fórmula da área: area = π . raio2
+//		Considere o valor de π = 3.14159
+
+		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 
-		int A, B, SOMA;
-
-		A = input.nextInt();
-		B = input.nextInt();
+		double area, pi, raio;
 		
-		SOMA = A + B;
+		pi = 3.14159;
 		
-		System.out.println("SOMA = "+ SOMA);
-
+		raio = input.nextDouble();
+		area = pi * (raio * raio);
+		
+		System.out.printf("A = %.4f\n", area);
 		input.close();
 	}
 
