f8a2479b8f23dececbd28251105f535c8ef73bf5
Iury Chagas <iurychagas@icloud.com>
2020-04-06 13:40:02 -0300

    seção 4: Exercício 05

--- Java-Intensive-hands-on/curso_programacao/src/Main.java
+++ Java-Intensive-hands-on/curso_programacao/src/Main.java
@@ -4,24 +4,33 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-//		Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
-//		hora e calcula o salário desse funcionário. 
+//		Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
+//		código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. 
 
-//		A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+//		Calcule e mostre o valor a ser pago.
 
 		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 
-		int Numero, Horas_trabalho;
-		float Valor_hora, Salary;
+		int cod1, cod2, n_peca1, n_peca2;
+		double v_unitario1, v_unitario2, valor_total1, valor_total2, valor_total_pecas;
 
-		Numero = input.nextInt();
-		Horas_trabalho = input.nextInt();
-		Valor_hora = input.nextFloat();
+		cod1 = input.nextInt();
+		n_peca1 = input.nextInt();
+		v_unitario1 = input.nextDouble();
 
-		Salary = Valor_hora * Horas_trabalho;
+		valor_total1 = n_peca1 * v_unitario1;
 
-		System.out.printf("NUMBER = %d%nSALARY = U$ %.2f%n", Numero, Salary);
+		cod2 = input.nextInt();
+		n_peca2 = input.nextInt();
+		v_unitario2 = input.nextDouble();
+
+		valor_total2 = n_peca2 * v_unitario2;
+
+		valor_total_pecas = valor_total1 + valor_total2;
+
+
+		System.out.printf("VALOR A PAGAR = R$ %.2f\n", valor_total_pecas);
 		input.close();
 	}
 
